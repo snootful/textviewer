23170810f4d25ce0a683882326da4e6d46e8086a
Skyline <goodbyecrazy@gmail.com>
2015-12-07 14:34:10 +0800

    Remove unnecessary code .

--- src/main/java/com/example/textviewer/MainActivity.java
+++ src/main/java/com/example/textviewer/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.textviewer;
 
 import android.content.Intent;
 import android.net.Uri;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
